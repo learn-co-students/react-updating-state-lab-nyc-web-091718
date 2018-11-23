caa6689294850413c056b72c4a5a77a2f3878451
TurtleFeeder <t.choong@yahoo.com>
2018-11-22 19:44:30 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,23 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+
+export default class DigitalClicker extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      timesClicked: 0,
+    };
+  }
+
+  handleClick = () => {
+    let newNum = this.state.timesClicked + 1
+    this.setState({timesClicked: newNum},     ()=>console.log(this.state.timesClicked))
+  }
+
+  render(){
+    return (
+      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+    )
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,58 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+export default class YouTubeDebugger extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  }
+
+  handleBitClick = () => {
+    console.log(this.state);
+    this.setState({
+      settings: Object.assign({}, this.state.settings, {
+        bitrate: 12
+      }),
+    }, ()=> console.log('new bitrate', this.state));
+
+    // this.setState({
+    //   settings: {
+    //     ...this.state.settings,
+    //     bitrate: 12
+    //   }
+    // },
+    // ()=>console.log('new bitrate',this.state))
+  } // end handleBitClick fn
+
+  handleResClick = () => {
+    console.log(this.state);
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.settings.video,
+          resolution: '720p'
+        }
+      }
+    }, ()=> console.log('new res', this.state))
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+      <button className='bitrate' onClick={this.handleBitClick}>BitRate</button>
+      <button className='resolution' onClick={this.handleResClick}>Resolution</button>
+      </React.Fragment>
+    )
+  }
+
+}
